4f26166c7005bb5f5639b8164615abdb357829be
Aakanksha <aakankshamalothia777@gmail.com>
2023-06-29 20:06:43 +0530

    changing file structure and modifing reusable components

--- socialui/src/pages/HomePage/HomePage.jsx
+++ socialui/src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,30 @@
 import React, { useEffect, useState } from "react";
 import Navbar from "../../components/navbar/Navbar";
 import AddPost from "../../components/AddPost/AddPost";
-import PostList from "../../components/PostList/index";
-import SinglePost from "../../components/SinglePost/SinglePost";
+import PostList from "../../components/PostList/PostList";
 import "./HomePage.css";
+
+
+import Person1 from '../../Assets/person1.webp';
+import Post2 from '../../Assets/post2.avif';
+
+const singlePostData = [
+  {
+    userImage: Person1,
+    userName: "Aakanksha Malothia",
+    DateTime: "25/10/2023",
+    userHandle: "@Aakanksha777",
+    postDesc: "Went to this hangout place, Bob's in Marathalli yesterday. The ambience is real good and the mocktails are really fresh.",
+    PostImage: Post2
+  }, {
+    userImage: Person1,
+    userName: "Aakanksha Malothia",
+    DateTime: "25/10/2023",
+    userHandle: "@Aakanksha777",
+    postDesc: "Went to this hangout place, Bob's in Marathalli yesterday. The ambience is real good and the mocktails are really fresh.",
+    PostImage: Post2
+  }
+]
 
 const HomePage = ({ user }) => {
   const { _id: userId } = user
@@ -26,11 +47,9 @@ const HomePage = ({ user }) => {
       <div className="post-feed-container">
         <AddPost userId={userId} />
         <div className="feed-card">
-          <PostList posts={posts} />
+          <PostList posts={singlePostData} />
         </div>
       </div>
-      <SinglePost />
-
     </div>
   );
 };
